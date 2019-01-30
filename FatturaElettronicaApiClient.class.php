529887cf1d92d1bf88acb075a599d062595609cb
Claudio C <c.clix@tiscali.it>
2019-01-30 13:09:30 +0100

    Update FatturaElettronicaApiClient.class.php

--- FatturaElettronicaApiClient.class.php
+++ FatturaElettronicaApiClient.class.php
@@ -54,7 +54,7 @@ class FatturaElettronicaApiClient {
 	
 	
 	/**
-	 * Riceve tutti gli agiornamenti dal SDI: documenti di fattura, note di credito/debito, ed esiti di consegna
+	 * Riceve tutti gli aggiornamenti dal SDI: documenti di fattura, note di credito/debito, ed esiti di consegna
 	 * @param string $partitaIva Per ottenere solo i documenti relativi ad una partita iva, tra quelli associati all'utenza
 	 * @return array ack=OK|KO - error=[eventuale errore] - data=array di array(partita_iva, ricezione, sdi_identificativo, sdi_messaggio, sdi_nome_file, sdi_fattura, sdi_fattura_xml, sdi_data_aggiornamento, sdi_stato)
 	 */
