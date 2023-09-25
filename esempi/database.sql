a58a1e973c3a604ca5d68b725371e47cc2db9e5f
Claudio C <c.clix@tiscali.it>
2023-09-25 12:42:04 +0200

    Update database.sql

--- esempi/database.sql
+++ esempi/database.sql
@@ -2,8 +2,9 @@
 
 CREATE TABLE `fatture_elettroniche` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `id_fattura` int(11) DEFAULT NULL, -- RIFERIMENTO ALLA FATTURA SUL PROPRIO DATABASE
-  `sdi_identificativo` varchar(36) CHARACTER SET utf8 NOT NULL,
+  `id_fattura` int(10) unsigned DEFAULT NULL, -- RIFERIMENTO ALLA FATTURA SUL PROPRIO DATABASE
+  `id_fattura_elettronica_api` bigint(20) unsigned DEFAULT NULL, -- identificativo "fattura elettronica api"
+  `sdi_identificativo` bigint(20) unsigned DEFAULT NULL,
   `sdi_stato` varchar(14) CHARACTER SET utf8 NOT NULL,
   `sdi_fattura` mediumtext CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `sdi_fattura_xml` mediumtext CHARACTER SET utf8 NOT NULL,
@@ -11,5 +12,6 @@ CREATE TABLE `fatture_elettroniche` (
   `sdi_messaggio` text CHARACTER SET utf8 NOT NULL,
   `sdi_nome_file` varchar(50) CHARACTER SET utf8 NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `id_fattura` (`id_fattura`)
+  KEY `id_fattura` (`id_fattura`),
+  KEY `id_fattura_elettronica_api` (`id_fattura_elettronica_api`)
 );
